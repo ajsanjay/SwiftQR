a59f09c753d0a037b42910ef9179abe858c34a99
Jayasabeen <aj.sabeen@gmail.com>
2024-01-02 17:02:04 +0530

    Refactor

--- BarcodeScanner/BarCodeScanner.swift
+++ BarcodeScanner/BarCodeScanner.swift
@@ -16,7 +16,7 @@ struct BarCodeScanner: View {
             VStack {
                 ScannerView(scannedCode: $viewModel.scannedCode,
                             alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .frame(maxHeight: 300)
                 
                 Spacer().frame(height: 60)
                 
